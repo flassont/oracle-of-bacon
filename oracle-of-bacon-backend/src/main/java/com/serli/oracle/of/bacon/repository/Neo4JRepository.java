d784dff93bd7f372d52bce9a3f981e81bb597e3c
Florian Lassont <florian.lassont@gmail.com>
2017-01-17 14:35:09 +0100

    Fetching connections from Neo4j database

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/Neo4JRepository.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/Neo4JRepository.java
@@ -1,26 +1,59 @@
 package com.serli.oracle.of.bacon.repository;
 
 
-import org.neo4j.driver.v1.AuthTokens;
-import org.neo4j.driver.v1.Driver;
-import org.neo4j.driver.v1.GraphDatabase;
-import org.neo4j.driver.v1.Session;
+import com.google.common.collect.Iterables;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.neo4j.driver.v1.*;
+import org.neo4j.driver.v1.types.Node;
+import org.neo4j.driver.v1.types.Path;
+import org.neo4j.driver.v1.types.Relationship;
 
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class Neo4JRepository {
     private final Driver driver;
 
     public Neo4JRepository() {
-        driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "password"));
+        driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "0rph@nBl@ck"));
     }
 
     public List<?> getConnectionsToKevinBacon(String actorName) {
-        Session session = driver.session();
-
         // TODO implement Oracle of Bacon
-        return null;
+        try (final Session session = driver.session()) {
+            final Path match = session.run(
+                    "MATCH (bacon:Actor {name:\"Bacon, Kevin (I)\"}),\n" +
+                            "      (actor:Actor {name: {actorName}}),\n" +
+                            "      p = shortestPath((bacon)-[:PLAYED_IN*]-(actor))\n" +
+                            "RETURN p",
+                    Values.parameters("actorName", actorName))
+                    .single()
+                    .get("p").asPath();
+            final LinkedList<GraphItem> items = new LinkedList<>();
+            match.nodes().forEach(node -> items.addFirst(Neo4JRepository.toGraphItem(node)));
+            match.relationships().forEach(relation -> items.addFirst(Neo4JRepository.toGraphItem(relation)));
+            return items;
+        }
+    }
+
+    private static GraphItem toGraphItem(final Node node) {
+        final String label = Iterables.get(node.labels(), 0);
+        final String valueLabel;
+        if (label.equalsIgnoreCase("Actor")) {
+            valueLabel = "name";
+        } else {
+            valueLabel = "title";
+        }
+
+        return new GraphNode(node.id(), node.get(valueLabel).asString(), label);
+    }
+
+    private static GraphItem toGraphItem(final Relationship rel) {
+        return new GraphEdge(rel.id(), rel.startNodeId(), rel.endNodeId(), rel.type());
     }
 
     private static abstract class GraphItem {
