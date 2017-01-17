a722b4767ccee7324b4c2291b369b2768b2ce54c
Florian Lassont <florian.lassont@gmail.com>
2017-01-17 11:11:39 +0100

    Fetching actor suggestions from ElasticSearch

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/ElasticSearchRepository.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/ElasticSearchRepository.java
@@ -1,10 +1,20 @@
 package com.serli.oracle.of.bacon.repository;
 
+import com.google.gson.JsonObject;
 import io.searchbox.client.JestClient;
 import io.searchbox.client.JestClientFactory;
+import io.searchbox.client.JestResult;
 import io.searchbox.client.config.HttpClientConfig;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
+import io.searchbox.core.Suggest;
+import io.searchbox.core.SuggestResult;
 
+import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
 
 public class ElasticSearchRepository {
 
@@ -28,8 +38,36 @@ public class ElasticSearchRepository {
     }
 
     public List<String> getActorsSuggests(String searchQuery) {
-        // TODO implent suggest
-        return null;
+        // TODO implement suggest
+        try {
+            String query = String.format(Locale.ROOT,
+                    "{\n" +
+                    "  \"query\": {\n" +
+                    "    \"match\": {\n" +
+                    "      \"name\": {\n" +
+                    "        \"query\":     \"%s\",\n" +
+                    "        \"fuzziness\": 1,\n" +
+                    "        \"operator\":  \"and\"\n" +
+                    "      }\n" +
+                    "    }\n" +
+                    "  }\n" +
+                    "}", searchQuery);
+            Search suggestion = new Search.Builder(query)
+                    .addIndex("bacon")
+                    .addType("actors")
+                    .build();
+            SearchResult result = this.jestClient.execute(suggestion);
+            if (result.isSucceeded()) {
+                return result.getHits(JsonObject.class)
+                        .stream()
+                        .map(hit -> hit.source.get("name").getAsString())
+                        .collect(Collectors.toList());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return Collections.emptyList();
     }
 
 
