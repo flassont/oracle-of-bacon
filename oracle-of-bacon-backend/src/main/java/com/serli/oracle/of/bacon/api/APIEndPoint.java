80674289654da40eb53bce494b249828683dbe9d
MaximeClaveau <claveau.mc@gmail.com>
2017-01-17 15:06:25 +0100

    Redis implementation

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/api/APIEndPoint.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/api/APIEndPoint.java
@@ -24,7 +24,7 @@ public class APIEndPoint {
 
     @Get("bacon-to?actor=:actorName")
     public String getConnectionsToKevinBacon(String actorName) {
-
+        redisRepository.addSearch(actorName);
         return "[\n" +
                 "{\n" +
                 "\"data\": {\n" +
@@ -73,11 +73,7 @@ public class APIEndPoint {
 
     @Get("last-searches")
     public List<String> last10Searches() {
-        return Arrays.asList("Peckinpah, Sam",
-                "Robbins, Tim (I)",
-                "Freeman, Morgan (I)",
-                "De Niro, Robert",
-                "Pacino, Al (I)");
+        return redisRepository.getLastTenSearches();
     }
 
     @Get("actor?name=:actorName")
--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/RedisRepository.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/RedisRepository.java
@@ -1,10 +1,25 @@
 package com.serli.oracle.of.bacon.repository;
 
 import java.util.List;
+import redis.clients.jedis.Jedis;
 
 public class RedisRepository {
+
+    private Jedis jedis;
+
+    final private String SEARCHES = "searchs";
+
+    public RedisRepository(){
+        jedis = new Jedis("localhost",6379);
+    }
+
+    public void addSearch(String search) {
+        jedis.lpush(SEARCHES, search);
+    }
+
     public List<String> getLastTenSearches() {
         // TODO implement last 10 searchs
-        return null;
+            jedis.ltrim(SEARCHES,0,9);
+        return jedis.lrange(SEARCHES, 0, 9);
     }
 }
